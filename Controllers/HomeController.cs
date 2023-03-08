e01bc5b98583a147b3cc6d52a0da2b2d731aca9a
reece <reecedevenney@gmail.com>
2023-03-07 19:11:43 -0500

    transfered premade html to pages

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -24,9 +24,16 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Profile()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View(
+            new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier }
+        );
     }
 }
